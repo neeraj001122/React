17a5883f395e1cd40f882f6d428f805c21630589
neeraj001122 <admobid0011>
2022-10-07 12:16:16 +0530

    reacttask7

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Expenseitem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenseitem></Expenseitem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem()
+{
+    var  ExpenseItem = <h2>My first react component</h2> 
+    var Food = <h4>food Rs.10</h4>
+    var petrol = <h4>petrol Rs.100</h4>
+    var movie = <h4>movie Rs.200</h4>
+    var arr = [ExpenseItem,Food,petrol,movie]
+    return arr
+    //
+}
+
+export default ExpenseItem;
